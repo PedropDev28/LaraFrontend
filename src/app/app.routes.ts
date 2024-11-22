839ec0fc32a4f3808165c04d36b296d10ba10c52
Pedro Rafael Lázaro Nevado <119618710+PedropDev28@users.noreply.github.com>
2024-11-22 09:18:39 +0000

    Agregar nuevas rutas para el componente de texto y perfil; implementar efecto de máquina de escribir en el componente de índice de audio.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { ClientTagComponent } from './audios/client-tag/client-tag.component';
 import { ClientIndexComponent } from './audios/client-index/client-index.component';
+import { ClientTextComponent } from './audios/client-text/client-text.component';
+import { UpdateComponent } from './users/update/update.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent, 'pathMatch': 'full'},
     {path: 'inicio', component: ClientIndexComponent},
+    {path: 'tag', component: ClientTagComponent},
+    {path: 'audio', component: ClientTextComponent},
+    {path: 'profile', component: UpdateComponent}
 ];
